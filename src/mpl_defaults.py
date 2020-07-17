45084f70ac5fdb33e2bc1f3fb04a21a172f11eac
Tyler Gordon <tagordon@uw.edu>
2020-07-17 13:26:44 -0700

    reorganizing files

---
+++ src/mpl_defaults.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as pl
+
+red = '#FE4365'
+blue = '#00A9FF'
+yellow = '#ECA25C'
+green = '#3F9778'
+darkblue = '#005D7F'
+
+pl.rc('xtick', labelsize=15)
+pl.rc('ytick', labelsize=15)
+pl.rc('axes', labelsize=25)
+pl.rc('axes', titlesize=30)
+pl.rc('legend', fontsize=20)
+pl.rc('lines', linewidth=4)
