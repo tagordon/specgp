6adfc54c768a0f54b04f1f25f226624043274771
Tyler Gordon <tagordon@uw.edu>
2020-07-20 15:45:34 -0700

    fixed

--- tests/test_means.py
+++ tests/test_means.py
@@ -7,13 +7,12 @@ import numpy as np
 
 def test_structure():
     t = np.linspace(0, 10, np.random.randint(100))
-    a = tt.dmatrix('a')
-    mu = sgp.means.KronMean(a)(t)
-    f = theano.function([a], mu)
     
     mu_lam = np.random.randint(10, size=10)
     kronmu = mu_lam[:, None] * np.ones(len(t))
-    assert np.all(f(kronmu) == np.tile(mu_lam, (len(t),)))
+    
+    gp_mean = sgp.means.KronMean(kronmu)(t).eval()
+    assert np.all(gp_mean == np.tile(mu_lam, (len(t),)))
     
 def test_in_gp():
     t = np.linspace(0, 10, np.random.randint(100))
@@ -21,6 +20,6 @@ def test_in_gp():
     kronmu = mu_lam[:, None] * np.ones(len(t))
     
     term = xo.gp.terms.SHOTerm(log_S0=0, log_w0=0, log_Q=0)
-    kernel = sgp.terms.KronTerm(term, alpha=[1, 2])
+    kernel = sgp.terms.KronTerm(term, alpha=np.linspace(1, 2, 10))
     gp = xo.gp.GP(kernel=kernel, x=t, diag=np.ones_like(kronmu), mean=kronmu, J=2)
     assert np.all(gp.mean_val.eval() == kronmu)
