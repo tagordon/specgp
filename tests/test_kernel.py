36b9ec1157d00e146e2edfe000b4bd5a55721055
Tyler Gordon <tagordon@uw.edu>
2020-07-20 18:16:21 -0700

    add test_values

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -4,30 +4,46 @@ import numpy as np
 import theano.tensor as tt
 import theano 
 
-print("look_here: {}".format(theano.config.compute_test_value))
-theano.config.compute_test_value = 'off'
 
-term = xo.gp.terms.SHOTerm(log_S0=0.0, log_w0=0.0, log_Q=0.0)
-kernel = sgp.terms.KronTerm(term, alpha=[1, 2, 3])
-t = np.linspace(0, 10, 10)
-diag = 1e-5 * np.ones((3, len(t)))
-Q = kernel.alpha[:, None]*kernel.alpha[None, :]
-gp = xo.gp.GP(kernel=kernel, diag=diag, mean=sgp.means.KronMean(np.zeros_like(diag)), x=t, J=2)
-K = tt.slinalg.kron(term.to_dense(t, np.zeros(len(t))), Q).eval()
-K = K + np.diag(np.concatenate(diag))
+logS0 = tt.scalar()
+logw0 = tt.scalar()
+logQ = tt.scalar()
+logS0.tag.test_value = -5.0
+logw0.tag.test_value = -2.0
+logQ.tag.test_value = 1.0
+term = xo.gp.terms.SHOTerm(S0=tt.exp(logS0), w0=tt.exp(logw0), Q=tt.exp(logQ))
+
+alpha = tt.vector()
+alpha.tag.test_value = np.array([1, 2, 3])
+kernel = sgp.terms.KronTerm(term, alpha=alpha)
+
+t = tt.vector()
+diag = tt.dmatrix()
+t.tag.test_value = np.linspace(0, 10, 10)
+diag.tag.test_value = 1e-5 * tt.ones((3, 10))
+mean = tt.dmatrix()
+mean.tag.test_value = 0.0 * tt.ones((3, 10))
+Q = alpha[:, None]*alpha[None, :]
+gp = xo.gp.GP(kernel=kernel, diag=diag, mean=sgp.means.KronMean(mean), x=t, J=2)
+K = tt.slinalg.kron(term.to_dense(t, tt.zeros_like(t)), Q)
+d = tt.vector()
+d.tag.test_value = 1e-5 * np.ones(30)
+K = K + tt.diag(d)
 
 def test_inverse():
-    z = np.random.randn(len(t)*3, 1)
-    y = np.dot(np.linalg.inv(K), z)
+    z = tt.dmatrix()
+    z.tag.test_value = np.random.randn(30, 1)
+    y = tt.dot(tt.nlinalg.matrix_inverse(K), z)
     y = tt.as_tensor_variable(y)
     assert tt.allclose(y, gp.apply_inverse(z))
     
 def test_determinant():
-    det = np.linalg.det(K)
+    det = tt.nlinalg.det(K)
     assert tt.allclose(tt.log(det), gp.log_det)
     
 def test_dot_l():
-    z = np.random.randn(len(t)*3, 1)
-    y = np.dot(np.linalg.cholesky(K), z)
+    z = tt.dmatrix()
+    z.tag.test_value = np.random.randn(30, 1)
+    y = tt.dot(tt.slinalg.cholesky(K), z)
     y = tt.as_tensor_variable(y)
     assert tt.allclose(y, gp.dot_l(z))
